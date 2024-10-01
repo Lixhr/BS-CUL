d158419e4e170d2dc0f48b110f818a203f217d68
Arthur Cabon <acabon@c1r3p8.42lehavre.fr>
2024-10-01 10:34:09 +0200

    bsq.h

--- includes/bsq.h
+++ includes/bsq.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   bsq.h                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cbeaufil <cbeaufil@student.42.fr>          +#+  +:+       +#+        */
+/*   By: acabon <acabon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/30 12:33:09 by cbeaufil          #+#    #+#             */
-/*   Updated: 2024/09/30 12:58:25 by cbeaufil         ###   ########.fr       */
+/*   Updated: 2024/10/01 10:24:00 by acabon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,5 +31,13 @@ typedef struct	s_max_sq_mat
 	int	y;
 	int	size;
 }	t_max_sq_mat;
+
+t_max_sq_mat	ft_max_sq(t_sq_mat sq_mat);
+void	ft_putsq(t_sq_mat sq_mat);
+int **ft_init_sq(int x, int y);
+int min_sq_nb(t_sq_mat sq_mat, int x, int y);
+void ft_sq_mat(char *str, t_sq_mat *sq_mat);
+int	check_str(t_sq_mat *sq_mat, char *str);
+int	verif_str(t_sq_mat sq_mat, char *str, int *pt, int *nb_col);
 
 #endif
