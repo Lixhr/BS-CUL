b618759fef22cd69afd8724a4002f40df0ca924e
Arthur Cabon <acabon@c1r3p8.42lehavre.fr>
2024-10-01 15:56:17 +0200

    norm

--- includes/bsq.h
+++ includes/bsq.h
@@ -6,7 +6,7 @@
 /*   By: acabon <acabon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/30 12:33:09 by cbeaufil          #+#    #+#             */
-/*   Updated: 2024/10/01 15:52:56 by acabon           ###   ########.fr       */
+/*   Updated: 2024/10/01 15:56:02 by acabon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,15 +33,15 @@ typedef struct s_max_sq_mat
 }	t_max_sq_mat;
 
 t_max_sq_mat	ft_max_sq(t_sq_mat sq_mat);
-int		ft_putsq(t_sq_mat sq_mat);
-int		**ft_init_sq(int x, int y);
-int		min_sq_nb(t_sq_mat sq_mat, int x, int y);
-void	ft_sq_mat(char *str, t_sq_mat *sq_mat);
-int		check_str(t_sq_mat *sq_mat, char *str);
-int		verif_str(t_sq_mat *sq_mat, char *str, int *pt, int *nb_col);
-int		solve(char *path);
-void	ft_putsq_test(t_sq_mat sq_mat);
-void	free_matrix(int **matrix, int size);
-void	free_data(char *map, int **matrix, int matrix_size);
+void			ft_sq_mat(char *str, t_sq_mat *sq_mat);
+void			ft_putsq_test(t_sq_mat sq_mat);
+void			free_matrix(int **matrix, int size);
+void			free_data(char *map, int **matrix, int matrix_size);
+int				ft_putsq(t_sq_mat sq_mat);
+int				**ft_init_sq(int x, int y);
+int				min_sq_nb(t_sq_mat sq_mat, int x, int y);
+int				check_str(t_sq_mat *sq_mat, char *str);
+int				verif_str(t_sq_mat *sq_mat, char *str, int *pt, int *nb_col);
+int				solve(char *path);
 
 #endif
