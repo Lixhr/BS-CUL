26b281b6fe8c5c65a18268b046c4c4e605559f5b
Charlie Beaufils <cbeaufil@c1r4p7.42lehavre.fr>
2024-10-01 17:58:31 +0200

    save

--- srcs/solver/solve.c
+++ srcs/solver/solve.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   solve.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: acabon <acabon@student.42.fr>              +#+  +:+       +#+        */
+/*   By: cbeaufil <cbeaufil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/01 08:22:25 by cbeaufil          #+#    #+#             */
-/*   Updated: 2024/10/01 15:45:46 by acabon           ###   ########.fr       */
+/*   Updated: 2024/10/01 17:40:19 by cbeaufil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,17 +19,16 @@ int	solve(char *path)
 
 	map = get_map(path);
 	if (map == NULL)
-		return (ft_putstr_error("get map error\n"));
+		return (ft_putstr_error("map error\n"));
 	if (check_charset(map))
-		return (free(map), ft_putstr_error("check_charset error\n"));
+		return (free(map), ft_putstr_error("map error\n"));
 	fill_charset(&matrix, map);
 	if (check_str(&matrix, map))
-		return (free(map), ft_putstr_error("check_str error\n"));
+		return (free(map), ft_putstr_error("map error\n"));
 	ft_sq_mat(map, &matrix);
 	if (ft_putsq(matrix))
 		return (free_data(map, matrix.sq, matrix.y),
-			ft_putstr_error("full map error\n"));
+			ft_putstr_error("map error\n"));
 	free_data(map, matrix.sq, matrix.y);
-	ft_putstr("\n");
 	return (0);
 }
