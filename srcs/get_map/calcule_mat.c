339cc964756ac8dd38facd551620ff3f834dc7f9
Arthur Cabon <acabon@c1r3p8.42lehavre.fr>
2024-10-01 10:27:37 +0200

    ajout put_mat et calc_mat.c

---
+++ srcs/get_map/calcule_mat.c
@@ -0,0 +1,76 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   calcule_mat.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acabon <acabon@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/01 09:51:32 by acabon            #+#    #+#             */
+/*   Updated: 2024/10/01 10:25:06 by acabon           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/bsq.h"
+
+
+int **ft_init_sq(int x, int y)
+{
+	int **mat_int;
+	int i;
+	int j;
+
+	mat_int = (int **)malloc(y * sizeof(int *));
+	i = 0;
+	while (i < y)
+	{
+		mat_int[i] = (int *)malloc(x * sizeof(int));
+		j = 0;
+		while ((i == 0 && j < x) || j == 0)
+		{
+			mat_int[i][j] = 0;
+			j++;
+		}
+		i++;
+	}
+	return mat_int;
+}
+
+int min_sq_nb(t_sq_mat sq_mat, int x, int y)
+{
+	int nb;
+
+	nb = sq_mat.sq[y - 1][x];
+	if (sq_mat.sq[y - 1][x - 1] < nb)
+		nb = sq_mat.sq[y - 1][x - 1];
+	if (sq_mat.sq[y][x - 1] < nb)
+		nb = sq_mat.sq[y][x - 1];
+	return nb;
+}
+
+void ft_sq_mat(char *str, t_sq_mat *sq_mat)
+{
+	int i;
+	int j;
+
+	(*sq_mat).sq = ft_init_sq((*sq_mat).x, (*sq_mat).y);
+	i = 1;
+	j = 1;
+	while (*str)
+	{
+		if (*str == '\n')
+		{
+			i++;
+			j = 0;
+		}
+		else if (*str == (*sq_mat).obstacle_c)
+		{
+			(*sq_mat).sq[i][j] = 0;
+		}
+		else if (*str == (*sq_mat).void_c)
+		{
+			(*sq_mat).sq[i][j] = 1 + min_sq_nb(*sq_mat, j, i);
+		}
+		j++;
+		str++;
+	}
+}
---
+++ srcs/get_map/put_mat.c
@@ -0,0 +1,91 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   put_mat.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acabon <acabon@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/01 09:52:39 by acabon            #+#    #+#             */
+/*   Updated: 2024/10/01 10:25:33 by acabon           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/bsq.h"
+
+t_max_sq_mat	ft_max_sq(t_sq_mat sq_mat)
+{
+	t_max_sq_mat	max_sq_mat;
+	int	i;
+	int	j;
+
+	max_sq_mat.size = sq_mat.sq[0][0];
+	i = 0;
+	while (i < sq_mat.y)
+	{
+		j = 0;
+		while (j < sq_mat.x)
+		{
+			if(max_sq_mat.size < sq_mat.sq[i][j])
+			{
+				max_sq_mat.size = sq_mat.sq[i][j];
+				max_sq_mat.x = j - max_sq_mat.size + 1;
+				max_sq_mat.y = i - max_sq_mat.size + 1;
+			}
+			j++;
+		}
+		i++;
+	}
+	return max_sq_mat;
+}
+
+// pour test :
+
+void	ft_putsq_test(t_sq_mat sq_mat)
+{
+	char	c;
+	int		i;
+	int		j;
+
+	i = 0;
+	while (i < sq_mat.y)
+	{
+		j = 0;
+		while (j < sq_mat.x)
+		{
+			if(j != 0)
+				write(1, " ", 1);
+			c = sq_mat.sq[i][j] + '0';
+			write(1, &c, 1);
+			j++;
+		}
+		write(1, "\n", 1);
+		i++;
+	}
+}
+
+void	ft_putsq(t_sq_mat sq_mat)
+{
+	t_max_sq_mat	max_sq_mat;
+	int			i;
+	int			j;
+
+	max_sq_mat = ft_max_sq(sq_mat);
+	i = 0;
+	while (++i < sq_mat.y)
+	{
+		j = 0;
+		while (++j < sq_mat.x)
+		{
+			// if(j != 0)
+			// 	write(1, " ", 1);
+			if (j >= max_sq_mat.x && j < max_sq_mat.x + max_sq_mat.size
+				&& i >= max_sq_mat.y && i < max_sq_mat.y + max_sq_mat.size)
+				write(1, "X", 1);
+			else if (sq_mat.sq[i][j] == 0)
+				write(1, "o", 1);
+			else
+				write(1, ".", 1);
+		}
+		write(1, "\n", 1);
+	}
+}
