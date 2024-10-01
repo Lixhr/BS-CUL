0039e2a89b9d73bd2366d7ca3b664ead82bf6ab4
Arthur Cabon <acabon@c1r3p8.42lehavre.fr>
2024-10-01 10:56:45 +0200

    resolution pb lignes

--- includes/bsq.h
+++ includes/bsq.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   bsq.h                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cbeaufil <cbeaufil@student.42.fr>          +#+  +:+       +#+        */
+/*   By: acabon <acabon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/30 12:33:09 by cbeaufil          #+#    #+#             */
-/*   Updated: 2024/10/01 10:40:01 by cbeaufil         ###   ########.fr       */
+/*   Updated: 2024/10/01 10:53:58 by acabon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,6 +38,6 @@ int **ft_init_sq(int x, int y);
 int min_sq_nb(t_sq_mat sq_mat, int x, int y);
 void ft_sq_mat(char *str, t_sq_mat *sq_mat);
 int	check_str(t_sq_mat *sq_mat, char *str);
-int	verif_str(t_sq_mat sq_mat, char *str, int *pt, int *nb_col);
+int	verif_str(t_sq_mat *sq_mat, char *str, int *pt, int *nb_col);
 int	solve(char *path);
 #endif
